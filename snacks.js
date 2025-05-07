12d07654f25c78de31ba9f295d47a4ef7acee57a
daviderigamonti1 <davide.rigamonti.01@gmail.com>
2025-05-07 18:03:08 +0200

    Snack 1

---
+++ snacks.js
@@ -0,0 +1,6 @@
+function getInitials(nomeCompleto) {
+    const [nome, cognome] = nomeCompleto.split(" ").filter(str => str !== '');
+    return `${nome.charAt(0).toUpperCase()} ${cognome.charAt(0).toUpperCase()}`;
+}
+
+module.exports = { getInitials }
